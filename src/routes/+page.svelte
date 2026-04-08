8fbf939d5c247822f98b1f9efb8c7b521fc31e64
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-08 23:13:51 +0200

    fetch and display dog data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
     import Header from "$lib/components/Header.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+    let { data } = $props()
 </script>
 
-<Header headerText='Main page'/>
+<Navbar />
+<Header headerText='Home page'/>
+<div class="mt-16 flex flex-col items-center">
+<p class="mb-2">Did you know?</p>
+    <p class="mb-5 italic">{data.fact}</p>
+    <img src={data.imageUrl} alt="dog" class="max-w-lg max-h-[500px]">
+</div>
