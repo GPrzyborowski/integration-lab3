a3e8e8391a2151e8b232d6b6f1d4edf6c57723dd
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-10 17:22:01 +0200

    use axios to fetch api data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,10 @@
+import axios from "axios"
+
 export async function load() {
-	const imgRes = await fetch('https://dog.ceo/api/breeds/image/random')
-	const textRes = await fetch('http://localhost:5173/api/dog/fact')
-	const img = await imgRes.json()
-	const text = await textRes.json()
+	const imgRes = await axios.get('https://dog.ceo/api/breeds/image/random')
+	const textRes = await axios.get('http://localhost:5173/api/dog/fact')
+	const img = imgRes.data
+	const text = textRes.data
 	return {
         imageUrl: img.message as string,
         fact: text.fact as string
