86a54a3a725b3e1cb3a15a89eeaca2d02c0786b2
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-11 20:37:20 +0200

    add and configure vitest for unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	test: {
+		environment: 'node',
+	},
 	plugins: [tailwindcss(), sveltekit()],
 })
