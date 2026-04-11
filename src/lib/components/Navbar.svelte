7cd931a40f48d4a745186f7ef1e7c80e04fd6776
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-11 02:30:11 +0200

    add posts route

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,4 +1,5 @@
 <nav class="mt-5 mb-2 mx-2 text-xl text-right">
 <a href="/" class="mx-4 p-1">Home</a>
 <a href="/weather" class="mx-4 p-1">Weather</a>
+<a href="/posts" class="mx-4 p-1">Posts</a>
 </nav>
