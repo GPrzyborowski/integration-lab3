4a73cea2322ba9cc854b6b5fcd7a043d92988509
Gabriel Przyborowski <gabrielprzyborowski@outlook.com>
2026-04-11 02:54:32 +0200

    add http status codes for error handling

--- src/routes/api/average-temp/+server.ts
+++ src/routes/api/average-temp/+server.ts
@@ -1,8 +1,10 @@
-import { json } from '@sveltejs/kit'
 import type { RequestEvent } from './$types'
 
 export async function GET({ url }: RequestEvent) {
 	const temperatures = url.searchParams.getAll('temperatures').map(Number)
+	if (temperatures.length === 0) {
+		return new Response(JSON.stringify({ error: 'No temperatures provided' }), { status: 400 })
+	}
 	const averageTemp = (temperatures.reduce((sum, current) => sum + current, 0) / temperatures.length).toFixed(1)
-	return new Response(JSON.stringify({ averageTemp }))
+	return new Response(JSON.stringify({ averageTemp }), { status: 200 })
 }
--- src/routes/api/dog/fact/+server.ts
+++ src/routes/api/dog/fact/+server.ts
@@ -3,5 +3,8 @@ import { dogFacts } from '$lib/data/dog-facts'
 
 export async function GET() {
 	const randomFact = dogFacts[Math.floor(Math.random() * dogFacts.length)]
-	return json({ fact: randomFact })
+	if (!randomFact) {
+		return json({ error: 'No facts available' }, { status: 500 })
+	}
+	return json({ fact: randomFact }, { status: 200 })
 }
